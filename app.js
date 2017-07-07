0b29f74e3d0fb5f92b2a8de38d12e2afb0f30d2c
regymso <regymso@gmail.com>
2017-07-07 12:55:29 +0300

    My-Website.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var person = {firstname:"<b>Regina</b>", lastname:"<b>Muthoni</b>",age:18,gender:"<b>female</b>"};
+let largeimage = document.createElement('img');
+let websites= document.querySelector('.websites');
 
-//console.log(person)
-//document.write(person.firstname);
-//document.write(person.age);
-//document.write(person.gender);
-//document.write(person.secondname);
-//$(document).ready(function(){//we used it when we want our code to excute after the body
+websites.appendChild(largeimage);
 
-//})
 
-$('.person').append('<p>First Name: ' + person.firstname +'</p>');
-$('.person').append('<p>Last Name: ' + person.lastname+'</p>');
-$('.person').append('<p> Gender: ' + person.gender+'</p>');
-$('.person').append('<p> Age: ' + person.age+'</p>');
+
+images.forEach(function (image) {
+
+	 image.onmousemove =function hover(event){
+	   let img = event.target;
+	   largeimage.src = img.src;
+	}
+
+})
